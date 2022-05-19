25eef6556c456603d35317c21b2115e971aa154b
MaenSZubaydi <maenzubaydi@gmail.com>
2022-05-20 05:23:47 +1000

    new

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ];
+
+function superbowlWin(array){
+    if(array.result===W){
+        return array.year;
+    }
+}
+
+const W = record.find(function superbowlWin(array) {
+    if(array.result==='W'){
+        val = array.year;
+        return val;
+        ;
+    }
+},val);
+
+
+console.log(W);
